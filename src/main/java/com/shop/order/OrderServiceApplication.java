971b7c043a22f47dc9cf1dcc6eaaed45aa016fee
Hai Tran Dong <hai.trandong@efundamentals.com>
2023-10-03 23:29:38 +0700

    rename package

--- src/main/java/com/shop/order/OrderServiceApplication.java
+++ src/main/java/com/shop/order/OrderServiceApplication.java
@@ -1,4 +1,4 @@
-package com.digital.order;
+package com.shop.order;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/main/java/com/shop/order/ServletInitializer.java
+++ src/main/java/com/shop/order/ServletInitializer.java
@@ -1,4 +1,4 @@
-package com.digital.order;
+package com.shop.order;
 
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
